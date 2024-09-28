d0024bbb8727ea9ec4cdeb1b9c3bb34e0f9a0287
nikunjcrest <nikunj.joshi@crestdata.ai>
2024-09-28 18:15:23 +0000

    prints

--- script.py
+++ script.py
@@ -3,6 +3,9 @@ import os
 def main():
     secret_key_1 = os.getenv('SECRET_KEY_1')
     secret_key_2 = os.getenv('SECRET_KEY_2')
+    print("The keys")
+    print("secret_key_1 = ")
+    print("secret_key_1 = ")
     print(f'Secret Key 1: {secret_key_1}')
     print(f'Secret Key 2: {secret_key_2}')
 
