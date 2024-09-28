48473980b4ad88cbdfcc25a5e84528d2696e4602
nikunjcrest <nikunj.joshi@crestdata.ai>
2024-09-28 18:21:49 +0000

    print

--- script.py
+++ script.py
@@ -3,11 +3,15 @@ import os
 def main():
     secret_key_1 = os.getenv('SECRET_KEY_1')
     secret_key_2 = os.getenv('SECRET_KEY_2')
+    secret_key_3 = os.getenv('SECRET_KEY_3')
     print("The Keys")
     print(f"{secret_key_1 = }")
     print(f"{secret_key_2 = }")
+    print(f"{secret_key_3 = }")
+
     print(f'Secret Key 1: {secret_key_1}')
     print(f'Secret Key 2: {secret_key_2}')
+    print(f'Secret Key 2: {secret_key_3}')
 
 if __name__ == "__main__":
     main()
